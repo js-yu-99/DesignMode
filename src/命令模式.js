46783559e23169ca56b1526ff201a9d03362f78d
wangyu <15804418440@163.com>
2021-08-23 22:51:35 +0800

    组合模式

--- src/命令模式.js
+++ src/命令模式.js
@@ -1,3 +1,21 @@
 /**
  * 命令模式：可以解耦请求调用者和请求接收者之间的关系
+ * 命令模式其实是回调函数的一个面向对象的替代品
  */
+
+var RefreshMenuBarCommand = function (receiver) {
+    return {
+        execute: function () {
+            receiver.refresh();
+        }
+    }
+};
+
+var setCommand = function (button, command) {
+    button.onclick = function () {
+        command.execute();
+    }
+};
+
+var refreshMenuBarCommand = RefreshMenuBarCommand(MenuBar);
+setCommand(button1, refreshMenuBarCommand);
